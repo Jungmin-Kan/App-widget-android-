afcff64384ccfa3ae938c3e4a91c6131b62cf486
Jungmin Kang <kgjm328@yonsei.ac.kr>
2022-07-27 12:57:06 +0900

    update app.js

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useEffect, useState, useRef  } from 'react';
+import { StyleSheet, Text, View, NativeModules, Button } from 'react-native';
 
+// const { SharedStorage } = NativeModules;
 export default function App() {
+  const [data, setData] = useState({text: '리액트로 부터 전달받은 앱'});
+
+  const _E = useRef();
+
+  // useEffect(()=>{
+    // console.log(SharedStorage);
+    // SharedStorage.set(
+    //   JSON.stringify(data)
+    //  );
+  // },[data]);
+
+  useEffect(()=>{
+  },[])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button 
+       title="Press me"
+       onPress={()=>{ 
+        setData({text: '변경해부리기'});
+        _E.current.props.onPress()
+      }}/>
+
+    <Button 
+       ref={_E}
+       title="Press me"
+       onPress={()=>{
+         console.log("hello click");
+         console.log(_E.current);
+      }}/>
     </View>
   );
 }
